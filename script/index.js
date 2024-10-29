73974a704c4b1b547ca9114f531dc49e4078d633
motacolina <joaceci21@gmail.com>
2024-10-29 12:52:13 -0500

    correccion
    modularicé el html para un adecuado manejo

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const menuItems = document.querySelectorAll('.menu ul li:not(.submenu)');
 menuItems.forEach(item => {
-    item.addEventListener('clic', (event) => {
+    item.addEventListener('click', (event) => {
 event.preventDefault();
 const submenu= item.querySelector('.submenu');
 submenu.classList.toggle('active');
