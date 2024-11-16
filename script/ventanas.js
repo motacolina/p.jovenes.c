751a7dfa609b0f43146591870de8e24ef627c636
motacolina <joaceci21@gmail.com>
2024-11-16 12:10:56 -0500

    Agrego archivos
    Incluyo archivos de css y js para la adaptación de ventanas flotantes

---
+++ script/ventanas.js
@@ -0,0 +1,24 @@
+function abrirt(){
+    document.getElementById("vent").style.display="block";
+}
+
+function abrird(){
+    document.getElementById("ventd").style.display="block";
+}
+
+function abrirtar(){
+    document.getElementById("ventar").style.display="block";
+ console.log("errores")
+}
+
+function cerrart(){
+    document.getElementById("vent").style.display="none";
+}
+
+function cerrard(){
+    document.getElementById("ventd").style.display="none";
+}
+
+function cerrartar(){
+    document.getElementById("ventar").style.display="none";
+}
